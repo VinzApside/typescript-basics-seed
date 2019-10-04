008b7711623a7d0ce0153410277ddadc36fccba7
Vinz <vincentballut@outlook.fr>
2019-10-04 13:50:21 +0200

    interface contracts with implements

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,46 @@
-type Pizza = { name: string; toppings: number };
-
-const pizza: Pizza = { name: "Pizza za", toppings: 4 };
-
-const serialized = JSON.stringify(pizza);
-
-function getNameFromJSON(obj: string) {
-  return (<Pizza>JSON.parse(obj)).name;
+interface SizesInterface {
+  availableSizes: string[];
 }
 
-function getNameFromJSONBetterWay(obj: string) {
-  return (JSON.parse(obj) as Pizza).name;
+abstract class Sizes implements SizesInterface {
+  constructor(protected sizes: string[]) {}
+
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+
+  get availableSizes() {
+    return this.sizes;
+  }
 }
-getNameFromJSON(serialized);
-getNameFromJSONBetterWay(serialized);
+
+interface PizzaInterface extends SizesInterface {
+  readonly name: string;
+  toppings: string[];
+  updateSizes(sizes: string[]): void;
+  addTopping(topping: string): void;
+}
+
+class Pizza extends Sizes implements PizzaInterface {
+  public toppings: string[] = [];
+
+  constructor(readonly name: string, sizes: string[]) {
+    super(sizes);
+  }
+
+  public updateSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+
+  public addTopping(topping: string) {
+    this.toppings.push(topping);
+  }
+}
+
+const pizza = new Pizza("Pepperoni", ["small", "medium"]);
+
+console.log(pizza.availableSizes);
+
+pizza.updateSizes(["large"]);
+
+console.log(pizza.availableSizes);

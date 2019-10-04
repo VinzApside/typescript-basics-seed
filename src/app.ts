0af6bffcf82053f15f3bf10397917e6b21df579c
Vinz <vincentballut@outlook.fr>
2019-10-04 11:34:50 +0200

    type assertions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello TypeScript!');
+type Pizza = { name: string; toppings: number };
+
+const pizza: Pizza = { name: "Pizza za", toppings: 4 };
+
+const serialized = JSON.stringify(pizza);
+
+function getNameFromJSON(obj: string) {
+  return (<Pizza>JSON.parse(obj)).name;
+}
+
+function getNameFromJSONBetterWay(obj: string) {
+  return (JSON.parse(obj) as Pizza).name;
+}
+getNameFromJSON(serialized);
+getNameFromJSONBetterWay(serialized);
